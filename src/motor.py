b19c333cf015d8c7f788131858508ca1a3c13ff4
yuba-k <160005925+yuba-k@users.noreply.github.com>
2025-10-24 01:46:42 +0900

    サーバサイドプログラム/モータ動作未試験

---
+++ src/motor.py
@@ -0,0 +1,92 @@
+import RPi.GPIO as GPIO
+import time
+#import configloading
+
+#import logwrite
+
+class Motor():
+    def __init__(self):
+#        self.config = configloading.Config_reader()
+#        self.log = logwrite.MyLogging()
+
+        self.duty = 80#self.config.reader("MOTOR","duty","intenger")
+        self.right_pwm = 13#self.config.reader("MOTOR","right_pwm","intenger")
+        self.left_pwm = 26#self.config.reader("MOTOR","left_pwm","intenger")
+        self.right_phase = 11#self.config.reader("MOTOR","right_phase","intenger")
+        self.left_phase = 19#self.config.reader("MOTOR","left_phase","intenger")
+
+        GPIO.setmode(GPIO.BCM)#setmodeでBCMを用いて指定することを宣言　#GPIOピン番号のこと！
+
+        self.direction = "stop"
+
+        self.setup_gpio()
+
+        self.initialize_motors()
+
+    def setup_gpio(self):
+        GPIO.setup(self.right_pwm,GPIO.OUT)#PWM出力
+        GPIO.setup(self.right_phase,GPIO.OUT)#デジタル出力
+        GPIO.setup(self.left_pwm,GPIO.OUT)#PWM出力
+        GPIO.setup(self.left_phase,GPIO.OUT)#デジタル出力
+
+    def initialize_motors(self):
+        self.right = GPIO.PWM(self.right_pwm,200)
+        self.left = GPIO.PWM(self.left_pwm,200)
+        # GPIO.output(self.right_phase,GPIO.HIGH)
+        # GPIO.output(self.left_phase,GPIO.HIGH)
+        self.right.start(0)
+        self.left.start(0)
+
+    def move(self,duty = None):
+        while True:
+            if duty == None:
+                duty = self.duty
+
+            self.adjust_duty_cycle(self.direction,duty)
+
+            #stop_time = time.time() + moter_active_time
+            self.right.ChangeDutyCycle(self.right_duty)
+            self.left.ChangeDutyCycle(self.left_duty)
+
+            self.right.ChangeDutyCycle(0)
+            self.left.ChangeDutyCycle(0)
+            time.sleep(0.5)#オーバーヒート対策/位置情報信号
+
+    def adjust_duty_cycle(self,direction,duty):
+        GPIO.output(self.right_phase,GPIO.HIGH)
+        GPIO.output(self.left_phase,GPIO.HIGH)
+        if direction == "forward":
+#            self.log.write(f"forward,Duty:{duty}","INFO")
+            self.right_duty = duty * 1.0
+            self.left_duty = duty
+        elif direction == "right" or direction == "search":
+#            self.log.write("right","INFO")
+            self.right_duty = duty * 0.6
+            self.left_duty = duty
+        elif direction == "left":
+#            self.log.write("left","INFO")
+            self.right_duty = duty * 1.0
+            self.left_duty = duty * 0.6
+        elif direction == "back":
+#            self.log.write("back","INFO")
+            self.right_duty = self.left_duty = duty
+            GPIO.output(self.right_phase,GPIO.LOW)
+            GPIO.output(self.left_phase,GPIO.LOW)
+        else:
+            self.right_duty = self.left_duty = 0
+        
+    def cleanup(self):
+        GPIO.cleanup()
+
+def main():
+    motor = Motor()
+    print("forward,right,left")
+    try:
+        while True:
+            direction =input("direction:")
+            motor.move(direction,3,100)
+    except KeyboardInterrupt:
+        motor.cleanup()
+
+if __name__ == "__main__":
+    main()
---
+++ src/server.py
@@ -0,0 +1,122 @@
+# server.py（Raspberry Pi側）
+import asyncio
+import websockets
+import cv2
+import base64
+import RPi.GPIO as GPIO
+from picamera2 import Picamera2
+import time
+import threading
+
+from adafruit_lsm6ds.lsm6ds33 import LSM6DS33
+import board
+import math
+
+import motor
+
+command = ""
+
+picam = Picamera2()
+picam.configure(picam.create_still_configuration(main={"format":"RGB888","size":(400,300)}))
+
+mv = motor.Motor()
+gyro = {"x":0,"y":0,"z":0}
+
+async def handler(websocket):
+    picam.start()
+    cap = picam.capture_array()
+    try:
+        while True:
+            # 映像送信
+            frame = picam.capture_array()
+            _, buffer = cv2.imencode('.jpg', frame)
+            img_str = base64.b64encode(buffer).decode('utf-8')
+            await websocket.send(img_str)
+
+            # コマンド受信
+#            try:
+#                command = await asyncio.wait_for(websocket.recv(), timeout=0.1)
+#                if command == "forward":
+#                    GPIO.output(18, GPIO.HIGH)
+#                elif command == "stop":
+#                    GPIO.output(18, GPIO.LOW)
+#                print(command)
+#            except asyncio.TimeoutError:
+#                pass
+    finally:
+        GPIO.cleanup()
+        picam.close()
+
+async def recvcommand(websocket):
+    global command
+    while True:
+        try:
+            command = await asyncio.wait_for(websocket.recv(),timeout=0.1)
+            mv.direction = command
+            print(command)
+        except asyncio.TimeoutError:
+            pass
+
+async def handler6asix(websocket):
+    tn = time.perf_counter()
+    try:
+        while True:
+            msg = f"{gyro['x']},{gyro['y']},{gyro['z']},{time.perf_counter()-tn:.2f}"
+            print(msg)
+            await websocket.send(msg) 
+            await asyncio.sleep(1)
+    finally:
+        print("全終了")
+
+def start_video_server():
+    async def video_server():
+        async with websockets.serve(handler,"0.0.0.0",8765):
+            print("映像サーバ起動(8765)")
+            await asyncio.Future()
+    asyncio.run(video_server())
+
+def start_other_servers():
+    async def main():
+        await asyncio.gather(
+                websockets.serve(handler6asix, "0.0.0.0", 9000),
+                websockets.serve(recvcommand,"0.0.0.0",9001),
+        )
+    # 永久待機
+        await asyncio.Future()
+    asyncio.run(main())
+
+def start_motor():
+    mv.move()
+
+def gyro_angle():
+    global gyro
+#    while True:
+#        print("動いてます")
+##        gyro["x"] = 10
+#        gyro["y"] = 5
+#        gyro["z"] = 1
+#        time.sleep(1)
+#    global angle
+    i2c = board.I2C()
+    sensor = LSM6DS33(i2c)
+    dt = 0.05
+    while True:
+        gyro["x"] = 0;gyro["y"] = 0;gyro["z"] = 0
+        while command == "right" or command == "left":
+            gyro_x, gyro_y, gyro_z = sensor.gyro
+            gyro["x"] += gyro_x * dt
+            gyro["y"] += gyro_y * dt
+            gyro["z"] += gyro_z * dt
+            time.sleep(dt)
+
+try:
+    threading.Thread(target=start_video_server,daemon=True).start()
+    threading.Thread(target=start_other_servers,daemon=True).start()
+    threading.Thread(target=start_motor).start()
+    threading.Thread(target=gyro_angle).start()
+    print("全サーバ起動")
+    while True:
+        time.sleep(1)
+finally:
+    mv.cleanup()
+    picam.close()
