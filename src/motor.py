34630c434a9b8433f681766d69c98a5d224a7321
yuba-k <160005925+yuba-k@users.noreply.github.com>
2025-10-24 20:19:05 +0900

    dutyを50に変更

--- src/motor.py
+++ src/motor.py
@@ -9,7 +9,7 @@ class Motor():
 #        self.config = configloading.Config_reader()
 #        self.log = logwrite.MyLogging()
 
-        self.duty = 80#self.config.reader("MOTOR","duty","intenger")
+        self.duty = 50#self.config.reader("MOTOR","duty","intenger")
         self.right_pwm = 13#self.config.reader("MOTOR","right_pwm","intenger")
         self.left_pwm = 26#self.config.reader("MOTOR","left_pwm","intenger")
         self.right_phase = 11#self.config.reader("MOTOR","right_phase","intenger")
@@ -41,20 +41,20 @@ class Motor():
         while True:
             if duty == None:
                 duty = self.duty
-
+            print(f"モータcmd{self.direction}")
             self.adjust_duty_cycle(self.direction,duty)
 
             #stop_time = time.time() + moter_active_time
             self.right.ChangeDutyCycle(self.right_duty)
             self.left.ChangeDutyCycle(self.left_duty)
 
-            self.right.ChangeDutyCycle(0)
-            self.left.ChangeDutyCycle(0)
+#            self.right.ChangeDutyCycle(0)
+#            self.left.ChangeDutyCycle(0)
             time.sleep(0.5)#オーバーヒート対策/位置情報信号
 
     def adjust_duty_cycle(self,direction,duty):
-        GPIO.output(self.right_phase,GPIO.HIGH)
-        GPIO.output(self.left_phase,GPIO.HIGH)
+        GPIO.output(self.right_phase,GPIO.LOW)
+        GPIO.output(self.left_phase,GPIO.LOW)
         if direction == "forward":
 #            self.log.write(f"forward,Duty:{duty}","INFO")
             self.right_duty = duty * 1.0
@@ -70,10 +70,12 @@ class Motor():
         elif direction == "back":
 #            self.log.write("back","INFO")
             self.right_duty = self.left_duty = duty
-            GPIO.output(self.right_phase,GPIO.LOW)
-            GPIO.output(self.left_phase,GPIO.LOW)
+            GPIO.output(self.right_phase,GPIO.HIGH)
+            GPIO.output(self.left_phase,GPIO.HIGH)
         else:
             self.right_duty = self.left_duty = 0
+            self.right.ChangeDutyCycle(0)
+            self.left.ChangeDutyCycle(0)
         
     def cleanup(self):
         GPIO.cleanup()
